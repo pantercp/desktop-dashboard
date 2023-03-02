f851d73ddeac840b00981cb642e43549271307c2
Christian Panter <pantercp@outlook.com>
2023-03-02 12:15:38 +0000

    Added function to draw header onto random image

--- inspiration_image.py
+++ inspiration_image.py
@@ -9,7 +9,36 @@ import csv
 import os
 from random import choice
 from datetime import date
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
+
+
+'''
+FUNCTION TO DRAW LOGO IN CORNER OF RANDOMLY SELECTED IMAGE
+'''
+def draw_logo(image):
+    
+    image_width, image_height = image.size
+    
+    LogoIm = Image.open(source_dir+'\images\logo\ResizedLogo.png')
+    Logo_Width, Logo_Height = LogoIm.size
+    draw.ellipse((15, 935, 145, 1065), ("black"))
+    # draw.rectangle((0, image_height-Logo_Height-10, Logo_Width+10, image_height), ("black"))
+    image.paste(LogoIm, (9, image_height-124), LogoIm)
+    # Save changes.
+    image.save(source_dir+'\output\\'+datesave+'.png')
+
+'''
+GETS TEXT DIMENSIONS FROM CHOSEN STING AND FONT
+'''
+def get_text_dimensions(text_string, font):
+
+    ascent, descent = font.getmetrics()
+
+    text_width = font.getmask(text_string).getbbox()[2]
+    text_height = font.getmask(text_string).getbbox()[3] + descent
+
+    return (text_width, text_height)
+
 
 
 source_dir = os.getcwd()
@@ -85,27 +114,48 @@ print(f'Photo Title: {image_title}')
 
 
 '''
-SAVE RANDOM IMAGE INTO OUTPUT FOLDER
+OPEN RANDOM IMAGE AND SAVE INTO OUTPUT FOLDER
 '''
 
+RandomImage = Image.open(source_dir+'\images\\'+str(image_choice))
+draw = ImageDraw.Draw(RandomImage)
 
-def draw_logo(image):
+draw_logo(RandomImage)
+
+
+'''
+DRAW HEADER IN TOP LEFT CORNER OF IMAGE
+'''
+
+# Locate & choose font format for Header
+fontsFolder = 'C:\Windows\Fonts'
+
+def draw_header(image):
+
+    head, subhead, disc = "Daily Positivity Generator", "Todays Date: "+dateformat,\
+        "*This is a randomly generated image"
+
+    FontProj = ImageFont.truetype(os.path.join(fontsFolder, 'arial.ttf'), 22)
+    FontDate = ImageFont.truetype(os.path.join(fontsFolder, 'arial.ttf'), 19)
+    FontDisc = ImageFont.truetype(os.path.join(fontsFolder, 'arial.ttf'), 14)
+    # Dimensions for date and info box
+    proj_width, proj_height = get_text_dimensions(head, FontProj)
+    date_width, date_height = get_text_dimensions(subhead, FontDate)
+    disc_width, disc_height = get_text_dimensions(disc, FontDisc)
     
-    LogoIm = Image.open(source_dir+'\images\logo\ResizedLogo.png')
-    Logo_Width, Logo_Height = LogoIm.size
-    draw.ellipse((15, 935, 145, 1065), ("black"))
-    # draw.rectangle((0, image_height-Logo_Height-10, Logo_Width+10, image_height), ("black"))
-    image.paste(LogoIm, (9, image_height-124), LogoIm)
-    # Save changes.
+    # Draw background for date and info box
+    draw.rectangle((0, 0, proj_width+10, proj_height+date_height+5), dark_clr)
+    draw.rectangle((0, proj_height+date_height+5, proj_width+10,
+                   proj_height+date_height+disc_height+5), light_clr)
+    
+    # Draw text for date and info box
+    draw.text((5, 5), head, light_clr, font=FontProj)
+    draw.text((5, 5+proj_height), subhead, light_clr, font=FontDate)
+    draw.text((5, 5+proj_height+date_height), disc, dark_clr, font=FontDisc)
+    
     image.save(source_dir+'\output\\'+datesave+'.png')
 
-RandomImage = Image.open(source_dir+'\images\\'+str(image_choice))
-image_width, image_height = RandomImage.size
-draw = ImageDraw.Draw(RandomImage)
-draw_logo(RandomImage)
-# RandomImage.save(source_dir+'\Output\\'+datesave+'.png')
 
-
-
-
-        
+dark_clr = (23, 42, 58)
+light_clr = (105, 209, 197)
+draw_header(RandomImage)
