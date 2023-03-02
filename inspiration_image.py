dcacf3cd81683a7813b661c17318b730764c9253
Christian Panter <pantercp@outlook.com>
2023-03-02 13:04:06 +0000

    Consolidated code into simpler functions

--- inspiration_image.py
+++ inspiration_image.py
@@ -39,96 +39,9 @@ def get_text_dimensions(text_string, font):
 
     return (text_width, text_height)
 
-
-
-source_dir = os.getcwd()
-
-
-'''
-GET DATE AND MAKE HEADER FOR IMAGE
-'''
-
-today = date.today()
-dateformat = today.strftime("%d/%m/%Y")
-datesave = today.strftime("%d-%m")
-
-print(f'\nTodays Date: {dateformat}\n*This is a randomly generated image')
-
-'''
-COLLECT NAMES OF ALLAH FROM CSV COMPILE INTO A LIST AND RANDOMLY SELECT ONE
-'''
-
-names = []
-file_name = r'\names.csv'
-
-# Collect values and append into a list
-with open(source_dir+file_name) as read_obj:
-    DictReader = csv.DictReader(read_obj)
-    for row in DictReader:
-        names.append(row)
-        
-# Return a randomly selected name
-name = choice(names)
-print()
-print(f'Name of Allah: {name["Name"]}\nTranslation: {name["Meaning"]}')
-
-
-'''
-COLLECT GRATITUDE AND AFFIRMATIONS FROM CSV COMPILE INTO A LIST AND RANDOMLY SELECT ONE
-'''
-
-gratitudes = []
-affirmations = []
-file_name = r'\inspire.csv'
-
-# Collect values and append into a list
-with open(source_dir+file_name) as read_obj:
-    DictReader = csv.DictReader(read_obj)
-    for row in DictReader:
-        gratitudes.append(row["Gratitude"])
-        affirmations.append(row["Affirmation"])
-
-# Return a randomly select gratitude and affirmation
-gratitude = choice(gratitudes)
-affirmation = choice(affirmations)
-print(f'Grateful for: {gratitude}')
-print(f'Affirmation of the day: {affirmation}')    
-
-
-'''
-RANDOMLY SELECT IMAGE FROM FOLDER
-'''    
-
-image_options = []
-
-for filename in os.listdir(source_dir+"\images"):  # Collect Image Keys
-    if filename.endswith(".png"):
-        image_options.append(filename)
-    else:
-        continue
-    
-image_choice = choice(image_options)
-image_title = image_choice.replace('.png', '')
-
-print(f'Photo Title: {image_title}')
-
-
-'''
-OPEN RANDOM IMAGE AND SAVE INTO OUTPUT FOLDER
-'''
-
-RandomImage = Image.open(source_dir+'\images\\'+str(image_choice))
-draw = ImageDraw.Draw(RandomImage)
-
-draw_logo(RandomImage)
-
-
 '''
 DRAW HEADER IN TOP LEFT CORNER OF IMAGE
 '''
-
-# Locate & choose font format for Header
-fontsFolder = 'C:\Windows\Fonts'
 
 def draw_header(image):
 
@@ -155,7 +68,103 @@ def draw_header(image):
     
     image.save(source_dir+'\output\\'+datesave+'.png')
 
+'''
+COLLECT NAMES OF ALLAH FROM CSV COMPILE INTO A LIST AND RANDOMLY SELECT ONE
+'''
 
-dark_clr = (23, 42, 58)
-light_clr = (105, 209, 197)
+def choose_name():
+    
+    names = []
+    file_name = r'\names.csv'
+
+    # Collect values and append into a list
+    with open(source_dir+file_name) as read_obj:
+        DictReader = csv.DictReader(read_obj)
+        for row in DictReader:
+            names.append(row)
+            
+    # Return a randomly selected name
+    name = choice(names)
+    return name
+
+'''
+COLLECT GRATITUDE AND AFFIRMATIONS FROM CSV RANDOMLY SELECT ONE OF EACH
+'''
+
+def choose_insp():
+    
+    gratitudes = []
+    affirmations = []
+    file_name = r'\inspire.csv'
+    
+    # Collect values and append into a list
+    with open(source_dir+file_name) as read_obj:
+        DictReader = csv.DictReader(read_obj)
+        for row in DictReader:
+            gratitudes.append(row["Gratitude"])
+            affirmations.append(row["Affirmation"])
+    
+    # Return a randomly select gratitude and affirmation
+    gratitude = choice(gratitudes)
+    affirmation = choice(affirmations)
+    
+    return gratitude, affirmation
+
+'''
+RANDOMLY SELECT IMAGE FROM FOLDER
+'''    
+
+def choose_image():
+        
+    image_options = []
+    
+    for filename in os.listdir(source_dir+"\images"):  # Collect Image Keys
+        if filename.endswith(".png"):
+            image_options.append(filename)
+        else:
+            continue
+        
+    image_choice = choice(image_options)
+    return image_choice
+    
+'''
+PROGRAM RUNS FROM HERE
+'''
+
+source_dir = os.getcwd()
+today = date.today()
+dateformat = today.strftime("%d/%m/%Y")
+datesave = today.strftime("%d-%m")
+# Return a random name of God
+name = choose_name()
+# Return inspiration variables
+gratitude, affirmation = choose_insp()
+# Choose inspiration image
+image_choice = choose_image()
+
+# Display all randomly selected variables
+print(f'\nTodays Date: {dateformat}\n*This is a randomly generated image')
+print(f'\nAffirmation of the day: {affirmation}')
+print(f'Name of Allah: {name["Name"]}\nTranslation: {name["Meaning"]}')
+print(f'Photo Title: {image_choice.replace(".png", "")}')
+print(f'Grateful for: {gratitude}')
+
+# Opens the inspiration image
+RandomImage = Image.open(source_dir+'\images\\'+str(image_choice))
+# Ability to draw on inspiration image
+draw = ImageDraw.Draw(RandomImage)
+# Puts CJPixel logo in bottom left corner
+draw_logo(RandomImage)
+# Locate & choose font format for drawing text
+fontsFolder = 'C:\Windows\Fonts'
+# Variables for colours
+dark_clr, light_clr = (23, 42, 58), (105, 209, 197)
+# Creates header in top left corner
 draw_header(RandomImage)
+
+
+'''
+REQUIRED FUNCTIONS
+'''
+
+# DRAW INSPIRATION TEXT
